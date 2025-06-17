b78e99acf673dd9bf2a07f9e68bac83cb681d095
M.Durga Rohith Kumar <maraganidurgarohith@gmail.com>
2025-06-17 11:12:32 +0530

    Add files via upload

---
+++ Dice.java
@@ -0,0 +1,10 @@
+package new_package;
+import java.util.Random;
+public class Dice{
+		public  int compute(String roll) {
+		Random rd =new Random();
+		int z;
+		z=rd.nextInt(1,7);
+		return z;
+	}
+}
